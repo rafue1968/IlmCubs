3aa851ecd1949d3901d3dea7361b0d428f265f99
rafue1968 <rafue1968@gmail.com>
2026-04-18 15:09:24 +0100

    refractor: modified the code on login route and OAuth callback URL

--- app/components/LoginButton.tsx
+++ app/components/LoginButton.tsx
@@ -25,7 +25,7 @@ export default function LoginButton({ className, onClickStart }: Props) {
       body: JSON.stringify({
         codeChallenge,
         state,
-        redirectUri: "https://ilm-cubs.vercel.app/oauth/callback",
+        redirectUri: "https://quran-companion-real-life-guidance-omega.vercel.app/oauth/callback",
       }),
     });
 
